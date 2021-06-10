48fa70de379ab8d5ecbe470a3083142014b24c2a
Cade Ellsworth <caedmd2100@gmail.com>
2021-06-09 21:16:50 -0700

    added cookies and logout functionality

--- server.js
+++ server.js
@@ -12,9 +12,23 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+const session = require('express-session');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+        db: sequelize
+    })
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use(require('./controllers/'));
 
